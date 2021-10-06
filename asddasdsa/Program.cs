d35cbbbe0db9ccee55b9dd281c1ee0778063f549
ato16 <ataceber@hotmail.com>
2021-10-06 19:44:06 +0300

    ilk github gönderim projem

---
+++ asddasdsa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace asddasdsa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
